5ffe95cd19e23fea5751671e07b0631c103b2259
vovanolo <vpankiv@softserveinc.com>
2023-10-27 11:10:38 +0300

    fix: task3 task4

--- tests/task3.test.js
+++ tests/task3.test.js
@@ -4,18 +4,18 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
-  await page.goto('http://127.0.0.1:5500/index.html'); // Замініть 'path-to-your-html-file.html' на шлях до вашого HTML-файлу
+  await page.goto('http://127.0.0.1:5500/index.html');
 
-  // Вибираємо елементи DOM за їх id
+
   const dropdown = await page.$('#dropdown');
   const deleteButton = await page.$('#deleteButton');
 
-  // Перевірка наявності елементів
+
   if (dropdown && deleteButton) {
-    // Вибираємо перший елемент випадаючого списку
+
     await page.select('#dropdown', 'option1');
 
-    // Клікаємо на кнопку "Видалити вибраний елемент"
+ 
     await deleteButton.click();
 
     const option1 = await page.$('option[value="option1"]');
